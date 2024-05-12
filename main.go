7134ac5782d8698dbb4ed10a0cd90c8c9cd2e6d2
cihanalici <cihan_cihan27@hotmail.com>
2024-05-12 05:54:11 +0300

    ~caching values for api-calls

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"time"
+
+	"github.com/cihanalici/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
+
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
